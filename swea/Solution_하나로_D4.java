508c92afed4578bee2c5b3d4a6afa75177a6cb52
SimJungUk <simju9397@gmail.com>
2021-03-24 20:39:15 +0900

    solve : 하나로 AC

--- swea/Solution_하나로_D4.java
+++ swea/Solution_하나로_D4.java
@@ -1,12 +1,17 @@
 package swea;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Solution_하나로_D4 {
     static BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
     static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    static int N, E;
+    static int N;
+    static double E;
+    static Island[] islands;
+    static Double[] costs;
+    static boolean[] visited;
 
     static class Island {
         int x;
@@ -21,14 +26,47 @@ public class Solution_하나로_D4 {
     static void input() throws IOException
     {
         N = Integer.parseInt(bufferedReader.readLine());
+        islands = new Island[N];
+        costs = new Double[N];
+        visited = new boolean[N];
+        Arrays.fill(costs, Double.MAX_VALUE);
+        StringTokenizer xLine = new StringTokenizer(bufferedReader.readLine());
+        StringTokenizer yLine = new StringTokenizer(bufferedReader.readLine());
         for(int i = 0; i < N; i++) {
-            StringTokenizer xy = new StringTokenizer(bufferedReader.readLine());
-            
+            islands[i] = new Island(Integer.parseInt(xLine.nextToken()), Integer.parseInt(yLine.nextToken()));
         }
+        E = Double.parseDouble(bufferedReader.readLine());
+    }
+
+    static double calc(Island from, Island to) {
+        return (Math.pow(from.x-to.x, 2) + Math.pow(from.y-to.y, 2)) * E;
     }
 
     static void solve(int t) throws IOException {
-
+        int minVertex = 0;
+        double min, result = 0;
+        costs[0] = 0.0;
+        for(int c = 0; c < N; c++) {
+            min = Double.MAX_VALUE;
+            minVertex = 0;
+            for(int i = 0; i < N; i++) {
+                if(!visited[i] && min > costs[i]) {
+                    min = costs[i];
+                    minVertex = i;
+                }
+            }
+            result += min;
+            visited[minVertex] = true;
+            for(int i = 0 ; i < N; i++) {
+                if(!visited[i]) {
+                    Double dis =  calc(islands[minVertex], islands[i]);
+                    if(costs[i] > dis) {
+                        costs[i] = dis;
+                    }
+                }
+            }
+        }
+        bufferedWriter.write("#"+t+" "+(long)Math.round(result)+"\n");
     }
 
     public static void main(String[] args) throws IOException {
@@ -37,5 +75,7 @@ public class Solution_하나로_D4 {
             input();
             solve(i);
         }
+        bufferedWriter.close();
+        bufferedReader.close();
     }
 }
