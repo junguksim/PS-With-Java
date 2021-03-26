3ac1cceb4c193467dc1cf48bd8ea462423a9d976
junguksim <simju9397@gmail.com>
2021-03-26 16:28:22 +0900

    solve : 다리만들기2 AC

--- boj/BFS_DFS/Main_다리만들기2_17472.java
+++ boj/BFS_DFS/Main_다리만들기2_17472.java
@@ -1,14 +1,11 @@
 package boj.BFS_DFS;
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main_다리만들기2_17472 {
     private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     private static BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-    private static int N,M, islandsCount;
+    private static int N,M, islandsCount, islandsNo = 2;
     private static int[][] map;
     private static ArrayList<Island> islands;
     private static int[][] adjIslands;
@@ -65,6 +62,24 @@ public class Main_다리만들기2_17472 {
         islands = new ArrayList<>();
     }
 
+    private static void printMap() {
+        for(int i = 0 ; i < N; i++) {
+            for(int j = 0 ; j < M; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void printAdjIslands() {
+        for(int i = 0 ; i < islandsCount; i++) {
+            for(int j = 0 ; j < islandsCount; j++) {
+                System.out.print(adjIslands[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     private static void setIslands() {
         for(int i = 0 ; i < N; i++) {
             for(int j = 0 ; j < M; j++) {
@@ -74,24 +89,28 @@ public class Main_다리만들기2_17472 {
             }
         }
         islandsCount = islands.size();
-        for(int i = 0 ; i < N; i++) {
-            for(int j = 0 ; j < M; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
+
         adjIslands = new int[islandsCount][islandsCount];
-        System.out.println(islands);
+        for(int i = 0 ; i < islandsCount; i++) {
+            for(int j = 0 ; j < islandsCount; j++) {
+                if(i != j) adjIslands[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        //System.out.println(islands);
+        findConnectableIslands();
+        System.out.println(getMst());
     }
 
     private static void setIsland(int x, int y) {
         ArrayList<Node> grounds = new ArrayList<>();
         Deque<Node> deque = new ArrayDeque<>();
         deque.offer(new Node(x, y));
+        grounds.add(new Node(x, y));
         while (!deque.isEmpty()) {
             Node node = deque.poll();
             int sx = node.x;
             int sy = node.y;
+            map[sx][sy] = islandsNo;
             for(int i = 0; i < 4; i++) {
                 int nx = sx + dx[i];
                 int ny = sy + dy[i];
@@ -99,10 +118,12 @@ public class Main_다리만들기2_17472 {
                 if(map[nx][ny] != 1) continue;
                 deque.offer(new Node(nx, ny));
                 grounds.add(new Node(nx, ny));
-                map[nx][ny] = 2;
+                map[nx][ny] = islandsNo;
             }
         }
-        islands.add(new Island(islandsCount++, grounds));
+        islands.add(new Island(islandsNo - 2, grounds));
+        islandsCount++;
+        islandsNo++;
     }
 
     private static void findConnectableIslands() {
@@ -112,16 +133,61 @@ public class Main_다리만들기2_17472 {
             for(Node fromGround : fromGrounds) {
                 int sx = fromGround.x;
                 int sy = fromGround.y;
+                //System.out.println(fromNo + " 번 섬에서, " + sx + " " + sy + " 에서 시작");
                 for(int d = 0; d < 4; d++) {
                     int nx = sx + dx[d];
                     int ny = sy + dy[d];
-                    while (nx >= 0 || ny >= 0 || nx < N || ny < M) {
-                        nx += dx[d];
-                        ny += dy[d];
+                    int bridgeLength = 1;
+                    while (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                        //System.out.println(nx + " " + ny);
+                        if(map[nx][ny] == 0) {
+                            nx += dx[d];
+                            ny += dy[d];
+                            bridgeLength++;
+                            continue;
+                        }
+                        // 섬을 만났다면
+                        if(bridgeLength == 2) break; // 거리가 1밖에 안된다면 다리 X
+                        int toNo = map[nx][ny] - 2;
+                        //System.out.println(fromNo + " " + toNo);
+                        adjIslands[fromNo][toNo] = Math.min(adjIslands[fromNo][toNo], bridgeLength - 1);
+                        adjIslands[toNo][fromNo] = Math.min(adjIslands[toNo][fromNo], bridgeLength - 1);
+                        break;
                     }
                 }
             }
         }
+    }
+
+    private static int getMst() {
+        boolean[] visited = new boolean[islandsCount];
+        int[] minEdge = new int[islandsCount];
+        Arrays.fill(minEdge, Integer.MAX_VALUE);
+        minEdge[0] = 0;
+        int minVertex = 0, min = 0, result = 0;
+        for(int c = 0; c < islandsCount; c++) {
+            min = Integer.MAX_VALUE;
+            minVertex = 0;
+
+            for(int i = 0 ; i < islandsCount; i++) {
+                if(!visited[i] && min > minEdge[i]) {
+                    min = minEdge[i];
+                    minVertex = i;
+                }
+            }
+            result += min;
+            visited[minVertex] = true;
+
+            for(int i = 0 ; i < islandsCount; i++) {
+                if(!visited[i] && adjIslands[minVertex][i] != 0 && adjIslands[minVertex][i] != Integer.MAX_VALUE && minEdge[i] > adjIslands[minVertex][i]) {
+                    minEdge[i] = adjIslands[minVertex][i];
+                }
+            }
+        }
+        for(int i = 0 ; i < islandsCount; i++) {
+            if(minEdge[i] == Integer.MAX_VALUE) return -1;
+        }
+        return result;
     }
 
     private static void solve() throws IOException {
