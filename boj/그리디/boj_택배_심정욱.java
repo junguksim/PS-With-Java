cac650656b3f455a08486bfcf835c4c658af730d
SimJungUk <simju9397@gmail.com>
2021-04-05 22:53:46 +0900

    solve : 택배 AC

--- boj/그리디/boj_택배_심정욱.java
+++ boj/그리디/boj_택배_심정욱.java
@@ -1,24 +1,38 @@
 package boj.그리디;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class boj_택배_심정욱 {
     static BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
     static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     static int N,C,M;
-    static Village[] villages;
+    static int[] villages;
+    static Box[] boxes;
 
-    static class Village {
+    static class Box implements Comparable<Box> {
         int sendNo;
         int receiveNo;
         int count;
 
-        public Village(int sendNo, int receiveNo, int count) {
+        public Box(int sendNo, int receiveNo, int count) {
             this.sendNo = sendNo;
             this.receiveNo = receiveNo;
             this.count = count;
+        }
+
+        @Override
+        public int compareTo(Box o) {
+            return this.receiveNo == o.receiveNo ? this.sendNo - o.sendNo : this.receiveNo - o.receiveNo ;
+        }
+
+        @Override
+        public String toString() {
+            return "Village{" +
+                    "sendNo=" + sendNo +
+                    ", receiveNo=" + receiveNo +
+                    ", count=" + count +
+                    '}';
         }
     }
     static void input() throws IOException {
@@ -26,18 +40,49 @@ public class boj_택배_심정욱 {
         N = Integer.parseInt(nc.nextToken());
         C = Integer.parseInt(nc.nextToken());
         M = Integer.parseInt(bufferedReader.readLine());
-        villages = new Village[N];
+        boxes = new Box[M];
+        villages = new int[N+1];
         for(int i = 0 ; i < M; i++) {
             StringTokenizer st = new StringTokenizer(bufferedReader.readLine());
-            villages[i] = (new Village(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
+            boxes[i] = (new Box(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken())));
         }
+        Arrays.sort(boxes);
     }
 
     static void solve() {
+        int boxCount = 0;
+        for(Box box : boxes) {
+            int sendNo = box.sendNo;
+            int receiveNo = box.receiveNo;
+            int count = box.count;
+            int max = 0;
+            boolean isLoad = true;
+            for(int i = sendNo; i < receiveNo; i++) {
+                if(villages[i] >= C) {
+                    isLoad = false;
+                    break;
+                }
+                max = Math.max(max, villages[i]);
+            }
+            if(isLoad) {
+                int unloads = C - max;
+                if(unloads > count) {
+                    unloads = count;
+                }
+                boxCount += unloads;
 
+                for(int i = sendNo; i < receiveNo; i++) {
+                    villages[i] += unloads;
+                }
+            }
+        }
+        System.out.println(boxCount);
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception{
+        input();
+        solve();
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
